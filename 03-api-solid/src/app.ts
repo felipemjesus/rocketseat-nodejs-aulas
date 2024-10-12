fd32f8e04355c4a8bc83c970471760aa8abebf5b
Felipe Martins <felipemjesus@outlook.com>
2024-10-11 23:07:58 -0300

    iniciando com prisma

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,6 +1,16 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'John Doe',
+    email: 'j@j.com',
+  },
+})
 
 app.get('/', () => {
   return 'API SOLID'
