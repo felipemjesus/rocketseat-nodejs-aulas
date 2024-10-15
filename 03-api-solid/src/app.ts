75066faab74dc64eb917e828ea1076f8789cac84
Felipe Martins <felipemjesus@outlook.com>
2024-10-15 19:32:11 -0300

    config log prisma dev

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,15 +1,27 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.post('/users', async (request, reply) => {
+  const createUserBody = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
 
-prisma.user.create({
-  data: {
-    name: 'John Doe',
-    email: 'j@j.com',
-  },
+  const { name, email, password } = createUserBody.parse(request.body)
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send(user)
 })
 
 app.get('/', () => {
