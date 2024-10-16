76eba30aa93dbb7b6eede45d1f84f60aef593b7d
Felipe Martins <felipemjesus@outlook.com>
2024-10-16 18:23:24 -0300

    authenticate user

---
+++ 03-api-solid/src/services/authenticate-service.ts
@@ -0,0 +1,34 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { InvalidCredentialsError } from './errors/invalid-credentiais-error'
+import { compare } from 'bcryptjs'
+
+interface AuthenticateRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateResponse {
+  user: User
+}
+
+export class AuthenticateService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateRequest): Promise<AuthenticateResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return { user }
+  }
+}
