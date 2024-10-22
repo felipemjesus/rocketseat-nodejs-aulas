474bbac8877b53c3bfa2f7d53df5b6bac9299104
Felipe Martins <felipemjesus@outlook.com>
2024-10-22 20:12:54 -0300

    test e2e get profile

--- 03-api-solid/src/http/controllers/users-controller.spec.ts
+++ 03-api-solid/src/http/controllers/users-controller.spec.ts
@@ -20,4 +20,31 @@ describe('UsersController (e2e)', () => {
 
     expect(response.statusCode).toEqual(201)
   })
+
+  it('should be able to get user profile', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const responseAuth = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const { token } = responseAuth.body
+
+    const response = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.user).toEqual(
+      expect.objectContaining({
+        email: 'johndoe@example.com',
+      }),
+    )
+  })
 })
