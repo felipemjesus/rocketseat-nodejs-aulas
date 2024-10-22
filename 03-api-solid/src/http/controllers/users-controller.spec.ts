307007fdec8d52635619d3908b62478c348029aa
Felipe Martins <felipemjesus@outlook.com>
2024-10-22 20:07:26 -0300

    test e2e authenticate

--- 03-api-solid/src/http/controllers/users-controller.spec.ts
+++ 03-api-solid/src/http/controllers/users-controller.spec.ts
@@ -2,7 +2,7 @@ import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import request from 'supertest'
 import { app } from '@/app'
 
-describe('Register (e2e)', () => {
+describe('UsersController (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
