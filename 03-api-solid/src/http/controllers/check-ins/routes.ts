32d035a54eda175dea44c511f431941d02b3a147
Felipe Martins <felipemjesus@outlook.com>
2024-10-23 18:54:18 -0300

    role base controller

--- 03-api-solid/src/http/controllers/check-ins/routes.ts
+++ 03-api-solid/src/http/controllers/check-ins/routes.ts
@@ -4,13 +4,18 @@ import { create } from './create/create-controller'
 import { validate } from './validate/validate-controller'
 import { metrics } from './metrics/metrics-controller'
 import { history } from './history/history-controller'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export const checkInsRoutes = async (app: FastifyInstance) => {
   app.addHook('onRequest', verifyJwt)
 
   app.post('/gyms/:gymId/check-ins', create)
 
-  app.patch('/check-ins/:checkInId/validate', validate)
+  app.patch(
+    '/check-ins/:checkInId/validate',
+    { onRequest: [verifyUserRole('ADMIN')] },
+    validate,
+  )
 
   app.get('/check-ins/metrics', metrics)
 
--- 03-api-solid/src/http/controllers/gyms/routes.ts
+++ 03-api-solid/src/http/controllers/gyms/routes.ts
@@ -3,11 +3,12 @@ import { FastifyInstance } from 'fastify'
 import { create } from './create/create-controller'
 import { search } from './search/search-controller'
 import { nearby } from './nearby/nearby-controller'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export const gymsRoutes = async (app: FastifyInstance) => {
   app.addHook('onRequest', verifyJwt)
 
-  app.post('/gyms', create)
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 
   app.get('/gyms/search', search)
 
