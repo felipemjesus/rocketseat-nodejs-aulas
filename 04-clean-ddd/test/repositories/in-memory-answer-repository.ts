ccb05b991ad5fb6fc11c6a5cf3929fc01fd283d8
Felipe Martins <felipemjesus@outlook.com>
2024-12-04 23:18:40 -0300

    Caso de uso: Listar respostas da pergunta

--- 04-clean-ddd/test/repositories/in-memory-answer-repository.ts
+++ 04-clean-ddd/test/repositories/in-memory-answer-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerRepository } from '@/domain/forum/application/repositories/answer-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
@@ -11,6 +12,17 @@ export class InMemoryAnswerRepository implements AnswerRepository {
     }
 
     return answer
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<Answer[]> {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
   }
 
   async create(answer: Answer) {
