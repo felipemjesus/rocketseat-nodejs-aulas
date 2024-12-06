525ed6c63e9b45beccb395f7da0273f4bb643b34
Felipe Martins <felipemjesus@outlook.com>
2024-12-05 21:44:01 -0300

    Caso de uso: Comentar na resposta

---
+++ 04-clean-ddd/src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,35 @@
+import { InMemoryAnswerCommentRepository } from 'test/repositories/in-memory-answer-comment-repository'
+import { CommentOnAnswerUseCase } from './comment-on-answer'
+import { InMemoryAnswerRepository } from 'test/repositories/in-memory-answer-repository'
+import { makeAnswer } from 'test/factories/make-answer'
+
+let inMemoryAnswerRepository: InMemoryAnswerRepository
+let inMemoryAnswerCommentRepository: InMemoryAnswerCommentRepository
+let commentOnAnswer: CommentOnAnswerUseCase
+
+describe('Create Comment On Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswerRepository = new InMemoryAnswerRepository()
+    inMemoryAnswerCommentRepository = new InMemoryAnswerCommentRepository()
+    commentOnAnswer = new CommentOnAnswerUseCase(
+      inMemoryAnswerRepository,
+      inMemoryAnswerCommentRepository,
+    )
+  })
+
+  it('should be able to create comment on answer', async () => {
+    const answer = makeAnswer()
+
+    await inMemoryAnswerRepository.create(answer)
+
+    await commentOnAnswer.execute({
+      answerId: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      content: 'Conteúdo do comentário',
+    })
+
+    expect(inMemoryAnswerCommentRepository.items[0].content).toEqual(
+      'Conteúdo do comentário',
+    )
+  })
+})
---
+++ 04-clean-ddd/src/domain/forum/application/use-cases/comment-on-answer.ts
@@ -0,0 +1,42 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { AnswerRepository } from '../repositories/answer-repository'
+import { AnswerComment } from '../../enterprise/entities/answer-comment'
+import { AnswerCommentRepository } from '../repositories/answer-comment-repository'
+
+interface CommentOnAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+  content: string
+}
+
+interface CommentOnAnswerUseCaseResponse {
+  answerComment: AnswerComment
+}
+
+export class CommentOnAnswerUseCase {
+  constructor(
+    private answerRepository: AnswerRepository,
+    private answerCommentRepository: AnswerCommentRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    answerId,
+    content,
+  }: CommentOnAnswerUseCaseRequest): Promise<CommentOnAnswerUseCaseResponse> {
+    const answer = await this.answerRepository.findById(answerId)
+    if (!answer) {
+      throw new Error('Answer not found')
+    }
+
+    const answerComment = AnswerComment.create({
+      authorId: new UniqueEntityId(authorId),
+      answerId: new UniqueEntityId(answerId),
+      content,
+    })
+
+    await this.answerCommentRepository.create(answerComment)
+
+    return { answerComment }
+  }
+}
