f911d7fba82e3a4311550e19b049e34de0211c35
Felipe Martins <felipemjesus@outlook.com>
2024-12-09 22:44:53 -0300

    Criando pergunta com anexos

--- 04-clean-ddd/src/domain/forum/application/use-cases/create-question.spec.ts
+++ 04-clean-ddd/src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryQuestionRepository } from 'test/repositories/in-memory-question-repository'
 import { CreateQuestionUseCase } from './create-question'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 
 let inMemoryQuestionRepository: InMemoryQuestionRepository
 let createQuestion: CreateQuestionUseCase
@@ -15,9 +16,15 @@ describe('Create Question', () => {
       authorId: '1',
       title: 'Nova pergunta',
       content: 'Conteúdo da pergunta',
+      attachmentsIds: ['1', '2'],
     })
 
     expect(result.isRight()).toBe(true)
     expect(inMemoryQuestionRepository.items[0]).toEqual(result.value?.question)
+    expect(inMemoryQuestionRepository.items[0].attachments).toHaveLength(2)
+    expect(inMemoryQuestionRepository.items[0].attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityId('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityId('2') }),
+    ])
   })
 })
