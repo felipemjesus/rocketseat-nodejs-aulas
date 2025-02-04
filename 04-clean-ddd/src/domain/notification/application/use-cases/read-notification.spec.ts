ed3291c66d0aef123d6a07ce753920614cc3eecd
Felipe Martins <felipemjesus@outlook.com>
2025-02-04 10:43:57 -0300

    Caso de uso: ler notificação

---
+++ 04-clean-ddd/src/domain/notification/application/use-cases/read-notification.spec.ts
@@ -0,0 +1,49 @@
+import { InMemoryNotificationRepository } from 'test/repositories/in-memory-notification-repository'
+import { ReadNotificationUseCase } from './read-notification'
+import { makeNotification } from 'test/factories/make-notification'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+
+let inMemoryNotificationRepository: InMemoryNotificationRepository
+let readNotification: ReadNotificationUseCase
+
+describe('Read Notification', () => {
+  beforeEach(() => {
+    inMemoryNotificationRepository = new InMemoryNotificationRepository()
+    readNotification = new ReadNotificationUseCase(
+      inMemoryNotificationRepository,
+    )
+  })
+
+  it('should be able to read a notification', async () => {
+    const notification = makeNotification()
+
+    inMemoryNotificationRepository.create(notification)
+
+    const result = await readNotification.execute({
+      recipientId: notification.recipientId.toString(),
+      notificationId: notification.id.toString(),
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryNotificationRepository.items[0].readAt).toEqual(
+      expect.any(Date),
+    )
+  })
+
+  it('should not be able to read a notification from another user', async () => {
+    const notification = makeNotification({
+      recipientId: new UniqueEntityId('recipient-1'),
+    })
+
+    await inMemoryNotificationRepository.create(notification)
+
+    const result = await readNotification.execute({
+      notificationId: notification.id.toString(),
+      recipientId: 'recipient-2',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
+  })
+})
