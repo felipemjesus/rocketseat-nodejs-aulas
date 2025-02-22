b87ede4c2d806e41f81c0432b648dc553393c8bd
Felipe Martins <felipemjesus@outlook.com>
2025-02-22 16:22:36 -0300

    Banco de dados isolado nos testes

---
+++ 05-nest-clean/test/setup-e2e.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config'
+import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { exec, execSync } from 'node:child_process'
+
+const prisma = new PrismaClient()
+
+const generateDatabaseURL = (schema: string) => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not set')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
+const schemaId = randomUUID()
+
+beforeAll(async () => {
+  const databaseURL = generateDatabaseURL(schemaId)
+
+  process.env.DATABASE_URL = databaseURL
+
+  execSync('npx prisma migrate deploy')
+})
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+  await prisma.$disconnect()
+})
