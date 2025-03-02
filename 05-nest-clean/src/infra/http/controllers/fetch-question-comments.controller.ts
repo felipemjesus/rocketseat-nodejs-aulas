6466448ec592cc9e3eaf8816fc91fb63e9aeade4
Felipe Martins <felipemjesus@outlook.com>
2025-03-02 12:34:52 -0300

    Controller: Listar Comentários da Pergunta

---
+++ 05-nest-clean/src/infra/http/controllers/fetch-question-comments.controller.ts
@@ -0,0 +1,43 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Query,
+} from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation.pipe'
+import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments'
+import { CommentPresenter } from '../presenters/comment-presenter'
+
+const pageQueryParamSchema = z.coerce.number().min(1).optional().default(1)
+
+const pageValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+type PageQueryParam = z.infer<typeof pageQueryParamSchema>
+
+@Controller('/questions/:questionId/comments')
+export class FetchQuestionCommentsController {
+  constructor(private fetchQuestionComments: FetchQuestionCommentsUseCase) {}
+
+  @Get()
+  async handle(
+    @Query('page', pageValidationPipe) page: PageQueryParam,
+    @Param('questionId') questionId: string,
+  ) {
+    const result = await this.fetchQuestionComments.execute({
+      questionId,
+      page,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+
+    const questionComments = result.value.questionComments.map(
+      CommentPresenter.toHTTP,
+    )
+
+    return { questionComments }
+  }
+}
