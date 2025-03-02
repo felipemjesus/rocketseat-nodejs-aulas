d3f68cad6fd92144f494068f62521c9536f23a80
Felipe Martins <felipemjesus@outlook.com>
2025-03-02 11:35:55 -0300

    Controller: Escolher Melhor Resposta

---
+++ 05-nest-clean/src/infra/http/controllers/choose-question-best-answer.controller.ts
@@ -0,0 +1,33 @@
+import {
+  BadRequestException,
+  Controller,
+  HttpCode,
+  Param,
+  Patch,
+} from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user.decorator'
+import { TokenPayload } from '@/infra/auth/jwt.strategy'
+import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-cases/choose-question-best-answer'
+
+@Controller('/answers/:answerId/choose-as-best')
+export class ChooseQuestionBestAnswerController {
+  constructor(
+    private chooseQuestionBestAnswer: ChooseQuestionBestAnswerUseCase,
+  ) {}
+
+  @Patch()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: TokenPayload,
+    @Param('answerId') answerId: string,
+  ) {
+    const result = await this.chooseQuestionBestAnswer.execute({
+      authorId: user.sub,
+      answerId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
