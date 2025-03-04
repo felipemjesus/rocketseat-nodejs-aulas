8f53b23537beb746b7422109fa84b472c41a3819
Felipe Martins <felipemjesus@outlook.com>
2025-03-03 22:11:21 -0300

    Controller comentário com autor

--- 05-nest-clean/src/infra/http/controllers/fetch-question-comments.controller.e2e-spec.ts
+++ 05-nest-clean/src/infra/http/controllers/fetch-question-comments.controller.e2e-spec.ts
@@ -61,9 +61,15 @@ describe('FetchQuestionCommentsController (e2e)', () => {
 
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({
-      questionComments: expect.arrayContaining([
-        expect.objectContaining({ content: 'Comment 1' }),
-        expect.objectContaining({ content: 'Comment 2' }),
+      comments: expect.arrayContaining([
+        expect.objectContaining({
+          content: 'Comment 1',
+          authorName: user.name,
+        }),
+        expect.objectContaining({
+          content: 'Comment 2',
+          authorName: user.name,
+        }),
       ]),
     })
   })
